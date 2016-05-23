3779d8ab0d9411bf0dd40577bbf3c71d13cd2225
Mikhail Brel <m@brel.me>
2016-05-23 12:13:26 +0500

    logging setup

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"flag"
+	"github.com/minya/gopushover"
 	"github.com/minya/goutils/web"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"os/user"
 	"path"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
+var logPath string
+
+func init() {
+	const (
+		defaultLogPath = "domofone.log"
+	)
+	flag.StringVar(&logPath, "logpath", defaultLogPath, "Path to write logs")
+}
+
 func main() {
+	flag.Parse()
+
+	SetUpLogger()
 	jar := web.NewJar()
 	transport := web.DefaultTransport(5000)
 	client := http.Client{
@@ -22,8 +39,7 @@ func main() {
 
 	_, enterErr := client.Get("http://domofon-e.ru/lk/enter")
 	if nil != enterErr {
-		fmt.Printf("Zalupa: %v", enterErr)
-		return
+		log.Fatalf("site is down: %v\n", enterErr)
 	}
 
 	user, _ := user.Current()
@@ -32,8 +48,7 @@ func main() {
 	var settings Settings
 	settingsBin, settingsErr := ioutil.ReadFile(settingsPath)
 	if nil != settingsErr {
-		fmt.Printf("Zalupa (read settings): %v \n", settingsErr)
-		return
+		log.Fatalf("read settings: %v \n", settingsErr)
 	}
 
 	json.Unmarshal(settingsBin, &settings)
@@ -45,8 +60,7 @@ func main() {
 
 	respGetData, errGetData := client.PostForm(loginUrl, data)
 	if nil != errGetData || respGetData.StatusCode != 200 {
-		fmt.Printf("Zalupa (getData): %v \n", enterErr)
-		return
+		log.Fatalf("getData: %v \n", errGetData)
 	}
 
 	petr, _ := client.Get("http://xn--e1aqefjh9f.xn--p1ai/lk/state/")
@@ -56,15 +70,96 @@ func main() {
 	reMoney, _ := regexp.Compile("<td class=\"lks03\"><b class=\"green\">(.+?) руб.</b>")
 	match := reMoney.FindStringSubmatch(html)
 
-	fmt.Printf(match[1] + "\n")
+	balance, errConvBal := strconv.Atoi(match[1])
+	if nil != errConvBal {
+		log.Fatalf("conv string balance to num: %v \n", errConvBal)
+	}
 
 	reFare, _ := regexp.Compile("<td class=\"lks03\">(.+?) руб./мес.</td>")
 	matchFare := reFare.FindStringSubmatch(html)
 
-	fmt.Printf(matchFare[1] + "\n")
+	fare, errConvFare := strconv.Atoi(matchFare[1])
+	if nil != errConvFare {
+		log.Fatalf("conv string fare to num: %v \n", errConvFare)
+	}
+
+	lastAction := GetLastAction()
+	if balance < 2*fare {
+		if lastAction != "notify" {
+			poSettings, poErr := gopushover.ReadSettings("~/.domofone/pushover.json")
+			if nil != poErr {
+				log.Fatalf("pushover settings: %v \n", poErr)
+			}
+
+			pushRes, pushErr := gopushover.SendMessage(
+				poSettings.Token, poSettings.User, "Domofon-e low balance", "Your balance is low")
+			if nil != pushErr {
+				log.Fatalf("Can't push: %v\n")
+			}
+
+			if nil != pushRes.Errors && len(pushRes.Errors) > 0 {
+				log.Fatalf("%v from pushover\n", pushRes.Errors)
+			}
+
+			lastAction = "notify"
+		}
+	} else {
+		if lastAction == "notify" {
+			lastAction = "pass"
+		}
+	}
+	SetLastAction(lastAction)
+}
+
+func SetUpLogger() {
+	logFile, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(logFile)
 }
 
 type Settings struct {
 	DomofonELogin    string
 	DomofonEPassword string
+}
+
+func GetLastAction() string {
+	content, errRead := ioutil.ReadFile(ExpandUserHome("~/.domofone/state.json"))
+	if nil != errRead {
+		return "pass"
+	}
+	var state State
+	errDeser := json.Unmarshal(content, &state)
+	if nil != errDeser {
+		return "pass"
+	}
+	return state.LastAction
+	return "pass"
+}
+
+func SetLastAction(value string) error {
+	state := State{}
+	state.LastAction = value
+	content, errSer := json.Marshal(state)
+	if nil != errSer {
+		return errSer
+	}
+	errWrite := ioutil.WriteFile(ExpandUserHome("~/.domofone/state.json"), content, 0660)
+	if nil != errWrite {
+		return errWrite
+	}
+	return nil
+}
+
+func ExpandUserHome(spath string) string {
+	if strings.Index(spath, "~/") != 0 {
+		return spath
+	}
+	user, _ := user.Current()
+	return path.Join(user.HomeDir, strings.TrimLeft(spath, "~/"))
+}
+
+type State struct {
+	LastAction string
 }
